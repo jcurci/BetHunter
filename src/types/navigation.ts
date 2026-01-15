93737c37349d2daedad19a636245b5e3e400120f
Ricardo <rick-queiroz@live.com>
2026-01-15 20:52:18 -0300

    feat: ajuste repetito

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -35,8 +35,6 @@ export type RootStackParamList = {
   PersonalityTestQuestion: undefined;
   MinhaConta: undefined;
   DetalhesPessoais: undefined;
-  PersonalityTestIntro: undefined;
-  PersonalityTestQuestion: undefined;
 };
 
 export type NavigationProp = RNNavigationProp<RootStackParamList>;
